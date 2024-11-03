6012303f50f5a033bd58829269eeb964e43386e0
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-03 18:13:49 +0530

    first commit
    Practice template

---
+++ bluehaze themplate/index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+            <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+            <!-- Bootstrap CSS v5.2.1 -->
+        <link             
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <div class="flex-container">
+                <div class="col1 sidebar_flex col_flex ">
+                    <h5 class="title-section">Sections</h5>
+                    <!-- computer stuff -->
+                    <h4>[ Computer  stuff ] </h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Programming</a></li>
+                        <li><a href="">Software</a></li>
+                        <li><a href="">Themes</a></li>
+                    </ul>
+
+                    <!-- Art stuff -->
+                    <h4>[ Art stuff ]</h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Gallary</a></li>
+                        <li><a href="">Software</a></li>
+                        <li><a href="">Forums</a></li>
+                    </ul>
+
+                    <!-- personal stuff -->
+                    <h4>[ Personal stuff ]</h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Photo Album</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Foobar</a></li>
+                    </ul>
+                </div>
+
+                <!-- Main contents -->
+                 <div class="main-content-wrapper">
+                    <div class="main-contents col2 col_flex">
+                        <h5 class="title-section">Main</h5>
+                        <!-- Main contents flex container -->
+                        <div class="main-contents-flex-container">
+                            <div class="section-vertical">
+                                about
+                            </div>
+                            
+                            <!-- Main contents flex container -->
+                            <div class="section-horizontal">
+                                Welcome to farmer fred and foobar jones' private pot growing pottery patch! Here you can examine some of the numerous species of potted pot plant Foobar Jones has to sell. He also specializes in dismantling the odd conspiracy and the even odder monopoly, although he prefers to do this only when they begin to infringe on his numerous ceramizied potted pot and pottery plants and plantations. The last time this occured was back in 1984 when.....
+                            </div>
+                        </div>
+
+                        <div class="main-contents-flex-container-2">
+                            <div class="section-vertical">
+                                news
+                            </div>
+                            <div class="section-horizontal">
+                                <h6>Turtle Dove Eats Bill Gates!</h6>
+                                    In a stunning, and very awesome surprise Bill Gates has been eaten by a partridge. Commonly called a turtle dove by native idiots of the planet Maroon, the partridge is commonly distributed as a gift. Often packaged in a pear tree. Although some consumer advocates have raised doubts as too the economics of this packaging method, they overlook the true danger of the matter. Partridges are murderers. In fact there have been at least a dozen confirmed killings by partridges in the last five weeks. News at 11.
+                                <h6>Doroc the Flying Llama!</h6>
+                                    In the realization of one of my life long childhood dreams, i finally became a true llama and joined the circus!!! W00000000000tttt!!! There i get to utilize my amazing and mystifying abilitys as I stun crowds of onlooks during my act as <strong>DOROC THE AMAZING! DOROC THE INCREDIBLE! DOROC THE FLYING LLAMA!!!</strong>
+                                <h6>IRC Drunkards</h6>
+                                    I'm just posting this reflect on the astounding prevalance of drunkards on IRC :) I'm sure all of you out there, who frequent IRC, have had "pleasure" of meeting many of these fine people who take it upon themselves to enlighten the populace as to their drunken state. Although many may respond negatively, the polite behavior is to challenge them to a game of quake and ask them to share the wealth. :)                        
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="main-contents-flex-container-2">
+                        <div class="section-vertical-col main-content-col">
+                            <h6>Random</h6>
+                            <div class="middle-left-contents">
+                                <strong>Revenge of the flying quote</strong>
+                                </br>
+                                Ep. 534.3: Revenge of the drunken programmer
+                            </div>
+                            <strong>!quote
+                            </br>
+                                &lt;mezza9&gt; (48/49) &lt;Lucifer-&gt; Lise, whats your job again? It began with a C i think 
+                                &lt;juno&gt; I dunno </br>
+                                &lt;juno&gt; I think it involes computers </br>
+                                &lt;Gliebster&gt; echo {lise is drunk} </br>
+                            </strong>
+                            </br>
+                        </div>
+                        <div class="section-horizontal-col main-content-col">
+                            <h6>Turtle Dove Eats Bill Gates!</h6>
+                        </div>
+                    </div>
+
+                    <div class="main-contents-flex-container-2">
+                        <div class="section-vertical-col main-content-col-full">
+                            [ <a href="">slashdot</a> ] [ <a href="">kuro5hin</a> ] [ <a href="">distrubted.net</a> ] [ <a href="">artaddiction.net</a> ] [ <a href="">lithiumdrift.com</a> ] [ <a href="">spacebarcowboy.com</a> <--    Coolness Award!! ]</br>
+                            Courtesy Open Web DesignThanks to Florida Vacation Homes
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
