079d7eed2bd758fe59d9f0ae7461adf2fdf2e7eb
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-08 19:49:06 +0530

    sample website template 02
    Sample website template 02

---
+++ andreas00/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <div class="main-page">
+        <header>
+            <div class="hero-banner">
+                <h3 class="hero-banner-site-name site-text">Universal Kinship</h3>
+                <p class="hero-banner-site-text  site-text">An open source website template ..</p>
+            </div>
+        </header>
+        <main>
+            <div class="contents">
+                <div class="left-sidebar col col-1">
+                    <ul class="main-ul sidebar-ul">
+                        <li class="menu-li"> 
+                            <a href="">First Page</a> 
+                        </li> 
+                        <li class="menu-li"> <a href="">Included Styles</a> 
+                            <ul class="sub-ul">
+                                <li class="menu-li sub-li">
+                                    <a href="">Single Column</a>
+                                    <a href="">2 Column</a>
+                                    <a href="">No Image</a>
+                                    <a href="">Empty Page</a>
+                                </li>
+                            </ul>
+                        </li> 
+                        <li class="menu-li"> <a href="">First page</a> </li> 
+                    </ul>
+
+                    <div class="latest-news">
+                        <h1> Latest News </h1>
+                        <p>
+                            Apr 20,2020:</br>
+                            Andreas00 v1.1 is released, with minor updates and fixes.</br>
+
+                            <a href="">Read more »</a>
+                        </p>
+                    </div>
+
+                </div>
+                <div class="middle-contents col col-2">
+
+                    <h1>andreas00 - a free website template!</h1>
+                    <p>
+                        This template could have been called "andreas01 - v2.0", because that is what it really is. I have taken my most popular website template and added the most requested features, such as a 2-column layout variation and support for sub-pages in the main menu. But I have also made other small modifications and added a few extras, like the background images in the menu buttons. I hope that you like what you see and that you find the template to be easy to work with. That is what it was made for!
+                    </p>
+
+                    <img src="images/test.jpg" width="150px" class="content-image left"/>
+                    <p>This template is released as open source, which means that you are free to use it in any way you may want to without any obligations, limitations or fees. I kindly ask that you leave the credits and the link to my website in the footer since it is a nice way of giving something back to me. But it is only a request, not any obligation.</p>
+
+                    <h1>Other versions and additional resources</h1>
+                    <p>
+                        This template is also available as a <strong>theme for WordPress</strong> and it has been ported 
+                        to a number of <strong>other blogs and CMS</strong> as well. New theme versions will be announced on my website whenever a new port is released.
+                    </p>
+
+                </div>
+                <div class="right-sidebar col col-3">
+                    <h1>Information</h1>
+                    <p>
+                        This template has a lot of additional styles and layout variations built into the stylesheet. To see these, use the navigation menu to view the different pages and layout examples.
+                    </p>
+
+                    <h1>Example Links</h1>
+                    <ul>
+                        <li><a href="http://andreasviklund.com/templates">More templates</a></li>
+                        <li><a href="http://jokkmokk.biz">Made in Jokkmokk</a></li>
+                        <li><a href="http://openwebdesign.org/">Open Web Design</a></li>
+                        <li><a href="http://oswd.org/">OSWD.org</a></li>
+                    </ul>
+
+                    <h1>Search Box</h1>
+                    <input type="text" name="search" class="searchbox" placeholder="(Demo Only)"/>
+
+
+                    <h1>Version</h1>
+                    <p>
+                    Universal Kinship v1.1 </br>
+                    (Apr 20, 2020)
+                    </p>
+                </div>
+            </div>
+        </main>
+        <hr/>
+        <footer class="footer">
+            <p>
+                Copyright © 2020 Sachin Mohite | Design by Andreas Viklund. | Courtesy Open Web DesignThanks to Florida Vacation Homes  
+            </p>
+        </footer>
+    </div>
+</body>
+</html>
