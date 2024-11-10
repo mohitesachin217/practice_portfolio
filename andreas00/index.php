bc572199bbf0fbd9e14328b200b664a3e4818205
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-10 13:55:14 +0530

    small changes

--- andreas00/index.php
+++ andreas00/index.php
@@ -1,100 +1,131 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="auto">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="styles.css" />
-</head>
-<body>
-    <div class="main-page">
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+            <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+            <!-- Bootstrap CSS v5.2.1 -->
+        <link             
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+
+    <body>
         <header>
-            <div class="hero-banner">
-                <h3 class="hero-banner-site-name site-text">Universal Kinship</h3>
-                <p class="hero-banner-site-text  site-text">An open source website template ..</p>
-            </div>
+            <!-- place navbar here -->
         </header>
         <main>
-            <div class="contents">
-                <div class="left-sidebar col col-1">
-                    <ul class="main-ul sidebar-ul">
-                        <li class="menu-li"> 
-                            <a href="">First Page</a> 
-                        </li> 
-                        <li class="menu-li"> <a href="">Included Styles</a> 
-                            <ul class="sub-ul">
-                                <li class="menu-li sub-li">
-                                    <a href="">Single Column</a>
-                                    <a href="">2 Column</a>
-                                    <a href="">No Image</a>
-                                    <a href="">Empty Page</a>
-                                </li>
-                            </ul>
-                        </li> 
-                        <li class="menu-li"> <a href="">First page</a> </li> 
+            <div class="flex-container">
+                <div class="col1 sidebar_flex col_flex ">
+                    <h5 class="title-section">Sections</h5>
+                    <!-- computer stuff -->
+                    <h4>[ Computer  stuff ] </h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Programming</a></li>
+                        <li><a href="">Software</a></li>
+                        <li><a href="">Themes</a></li>
                     </ul>
 
-                    <div class="latest-news">
-                        <h1> Latest News </h1>
-                        <p>
-                            Apr 20,2020:</br>
-                            Andreas00 v1.1 is released, with minor updates and fixes.</br>
+                    <!-- Art stuff -->
+                    <h4>[ Art stuff ]</h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Gallary</a></li>
+                        <li><a href="">Software</a></li>
+                        <li><a href="">Forums</a></li>
+                    </ul>
 
-                            <a href="">Read more »</a>
-                        </p>
+                    <!-- personal stuff -->
+                    <h4>[ Personal stuff ]</h4>
+                    <ul class="sidebar_flex_submenu">
+                        <li><a href="">Photo Album</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Foobar</a></li>
+                    </ul>
+                </div>
+
+                <!-- Main contents -->
+                 <div class="main-content-wrapper">
+                    <div class="main-contents col2 col_flex">
+                        <h5 class="title-section">Main</h5>
+                        <!-- Main contents flex container -->
+                        <div class="main-contents-flex-container">
+                            <div class="section-vertical">
+                                about
+                            </div>
+                            
+                            <!-- Main contents flex container -->
+                            <div class="section-horizontal">
+                                Welcome to farmer fred and foobar jones' private pot growing pottery patch! Here you can examine some of the numerous species of potted pot plant Foobar Jones has to sell. He also specializes in dismantling the odd conspiracy and the even odder monopoly, although he prefers to do this only when they begin to infringe on his numerous ceramizied potted pot and pottery plants and plantations. The last time this occured was back in 1984 when.....
+                            </div>
+                        </div>
+
+                        <div class="main-contents-flex-container-2">
+                            <div class="section-vertical">
+                                news
+                            </div>
+                            <div class="section-horizontal">
+                                <h6>Turtle Dove Eats Bill Gates!</h6>
+                                    In a stunning, and very awesome surprise Bill Gates has been eaten by a partridge. Commonly called a turtle dove by native idiots of the planet Maroon, the partridge is commonly distributed as a gift. Often packaged in a pear tree. Although some consumer advocates have raised doubts as too the economics of this packaging method, they overlook the true danger of the matter. Partridges are murderers. In fact there have been at least a dozen confirmed killings by partridges in the last five weeks. News at 11.
+                                <h6>Doroc the Flying Llama!</h6>
+                                    In the realization of one of my life long childhood dreams, i finally became a true llama and joined the circus!!! W00000000000tttt!!! There i get to utilize my amazing and mystifying abilitys as I stun crowds of onlooks during my act as <strong>DOROC THE AMAZING! DOROC THE INCREDIBLE! DOROC THE FLYING LLAMA!!!</strong>
+                                <h6>IRC Drunkards</h6>
+                                    I'm just posting this reflect on the astounding prevalance of drunkards on IRC :) I'm sure all of you out there, who frequent IRC, have had "pleasure" of meeting many of these fine people who take it upon themselves to enlighten the populace as to their drunken state. Although many may respond negatively, the polite behavior is to challenge them to a game of quake and ask them to share the wealth. :)                        
+                            </div>
+                        </div>
                     </div>
 
-                </div>
-                <div class="middle-contents col col-2">
+                    <div class="main-contents-flex-container-2">
+                        <div class="section-vertical-col main-content-col">
+                            <h6>Random</h6>
+                            <div class="middle-left-contents">
+                                <strong>Revenge of the flying quote</strong>
+                                </br>
+                                Ep. 534.3: Revenge of the drunken programmer
+                            </div>
+                            <strong>!quote
+                            </br>
+                                &lt;mezza9&gt; (48/49) &lt;Lucifer-&gt; Lise, whats your job again? It began with a C i think 
+                                &lt;juno&gt; I dunno </br>
+                                &lt;juno&gt; I think it involes computers </br>
+                                &lt;Gliebster&gt; echo {lise is drunk} </br>
+                            </strong>
+                            </br>
+                        </div>
+                        <div class="section-horizontal-col main-content-col">
+                            <h6>Turtle Dove Eats Bill Gates!</h6>
+                        </div>
+                    </div>
 
-                    <h1>andreas00 - a free website template!</h1>
-                    <p>
-                        This template could have been called "andreas01 - v2.0", because that is what it really is. I have taken my most popular website template and added the most requested features, such as a 2-column layout variation and support for sub-pages in the main menu. But I have also made other small modifications and added a few extras, like the background images in the menu buttons. I hope that you like what you see and that you find the template to be easy to work with. That is what it was made for!
-                    </p>
-
-                    <img src="images/test.jpg" width="150px" class="content-image left"/>
-                    <p>This template is released as open source, which means that you are free to use it in any way you may want to without any obligations, limitations or fees. I kindly ask that you leave the credits and the link to my website in the footer since it is a nice way of giving something back to me. But it is only a request, not any obligation.</p>
-
-                    <h1>Other versions and additional resources</h1>
-                    <p>
-                        This template is also available as a <strong>theme for WordPress</strong> and it has been ported 
-                        to a number of <strong>other blogs and CMS</strong> as well. New theme versions will be announced on my website whenever a new port is released.
-                    </p>
-
-                </div>
-                <div class="right-sidebar col col-3">
-                    <h1>Information</h1>
-                    <p>
-                        This template has a lot of additional styles and layout variations built into the stylesheet. To see these, use the navigation menu to view the different pages and layout examples.
-                    </p>
-
-                    <h1>Example Links</h1>
-                    <ul>
-                        <li><a href="http://andreasviklund.com/templates">More templates</a></li>
-                        <li><a href="http://jokkmokk.biz">Made in Jokkmokk</a></li>
-                        <li><a href="http://openwebdesign.org/">Open Web Design</a></li>
-                        <li><a href="http://oswd.org/">OSWD.org</a></li>
-                    </ul>
-
-                    <h1>Search Box</h1>
-                    <input type="text" name="search" class="searchbox" placeholder="(Demo Only)"/>
-
-
-                    <h1>Version</h1>
-                    <p>
-                    Universal Kinship v1.1 </br>
-                    (Apr 20, 2020)
-                    </p>
+                    <div class="main-contents-flex-container-2">
+                        <div class="section-vertical-col main-content-col-full">
+                            [ <a href="">slashdot</a> ] [ <a href="">kuro5hin</a> ] [ <a href="">distrubted.net</a> ] [ <a href="">artaddiction.net</a> ] [ <a href="">lithiumdrift.com</a> ] [ <a href="">spacebarcowboy.com</a> <--    Coolness Award!! ]</br>
+                            Courtesy Open Web DesignThanks to Florida Vacation Homes
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         </main>
-        <hr/>
-        <footer class="footer">
-            <p>
-                Copyright © 2020 Sachin Mohite | Design by Andreas Viklund. | Courtesy Open Web DesignThanks to Florida Vacation Homes  
-            </p>
+        <footer>
+            <!-- place footer here -->
         </footer>
-    </div>
-</body>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
