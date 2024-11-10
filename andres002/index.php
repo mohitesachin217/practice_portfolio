27e2cec865821609e7d04c51c77c95dafda58564
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-10 12:23:16 +0530

    andreas002

---
+++ andres002/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="warp">
+        <header>
+            <div class="top-header">
+                <span class="site-name">Universal Kinship</span>
+                <div class="column-menu-2">New: <a href="">2-column version</a> included!</div>
+            </div>
+            <img src="images/front.jpg" class="hero-image" />
+        </header>
+        <main>
+            <div class="contents">
+                <div class="left-sidebar">
+                    <ul class="sidebar-menu">
+                        <li class="sidebar-li"><a href="">Front Page</a></li>
+                        <li class="sidebar-li"><a href="">Included Layouts</a></li>
+                        <li class="sidebar-li"><a href="">3 Column Layout</a></li>
+                        <li class="sidebar-li"><a href="">2 Column Layout</a></li>
+                        <li class="sidebar-li"><a href="">Sub-Page Demo</a>
+                            <ul class="sub-sidebar-menu">
+                                <li class="sub-sidebar-li"><a href="">Sub-Page 1</a></li>
+                                <li class="sub-sidebar-li"><a href="">Sub-Page 2</a></li>
+                                <li class="sub-sidebar-li"><a href="">Sub-Page 3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Sample Page</a></li>
+                    </ul>
+                    <div class="right-sidebar-latest-news">
+                        <h3>Latest news:</h3>
+                        <h4>June 25, 2006:</h4>
+                        <p>
+                            One year anniversary of andreas01! 
+                            I celebrate it by releasing a new version (v2.0) 
+                            with new features and lots of improvements.
+                        </p>
+                        <h4>June 25, 2005:</h4>
+                        <p>
+                            Andreas releases his first free XHTML/CSS website template: andreas01 v1.0.
+                        </p>
+                        <a href="" style="float:right;display:block;">Read More..</a>
+                    </div>
+                </div>
+                <div class="contents-middle">
+                    <h3>Welcome to andreas 02!</h3>
+                    <p>
+                        This is an open source XHTML/CSS website template, a great tool that makes it possible for people with no web design experience to build high-quality websites. The template is free to download, and you can modify it freely and use it in any way you want to. There are no obligations and no limitations, although I kindly ask you to leave the original design credits in the footer to support my free template design work!
+                    </p>
+
+                    <img src="images/test2.jpg" class="contents-image" alt="" width="120px">
+                    <p class="contents-image-text">
+                        The andreas01 template is a very simple but also very useful standard web layout: a fixed-width 3-column page with a header and a footer. There are thousands of variations of this layout, and this is no perfect solution. But it should be a great starting point for you to customize, play with and add your own personal touch to. And there is a 2-column version included as well if you prefer that.
+                    </p>
+
+                    <h3>Open source web design </h3>
+                    <p>
+                        If you like this layout and would like to use it in any way, you are free to do so. All I ask for is that you leave the "Design by Andreas Viklund" link in the footer of the site, but that is only a request and not any obligation. If you choose to remove the credits and use the template for commercial purposes, I encourage you to consider a donation to support my work: <a href="" style="text-decoration:none;">Make a donation through PayPal!</a>
+                    </p>
+                    <p style="text-decoration:none;">Good luck with your new website!</p>
+                </div>
+                <div class="right-sidebar">
+                    <h3>More info:</h3>
+                    <p>
+                    This is the third column, and it can be used in many different ways. You can put links, images, short notes or contact information here just to give a few examples. Filler content has been added here to give an idea of what it can look like. It can also be <a href="" style="text-decoration:none;">removed</a> to give more space for the main content.
+                    </p>
+
+                    <h3>Links:</h3>
+                    <a href="" class="sidebar-link">Website templates</a>
+                    <a href="" class="sidebar-link">Made in Jokkmokk</a>
+                    <a href="" class="sidebar-link">Valid XHTML</a>
+                    <a href="" class="sidebar-link">Valid CSS</a>
+                    
+                    <h3>Version:</h3>
+                    andreas01 v2.0</br>
+                    (Jun 25, 2006)  </br>
+                    <img src="images/test.jpg" width="150px" style="margin:5px;"></img>
+
+                </div>
+            </div>
+        </main>
+        <hr>
+        <footer>
+            <div class="footer-div">
+            Copyright © 2006 Your Name | Thanks: <a href="">Digital Flowers</a> | Design by <a href=""> Andreas Viklund </a> </br> Courtesy <a href="">Open Web Design </a> Thanks to <a href=""> Florida Vacation Homes </a>
+            </div>
+            
+        </footer>
+    </div>
+</body>
+</html></a>
