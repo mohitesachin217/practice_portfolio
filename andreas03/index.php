f13e5bdfd66a4a89f3611203d4a96b3de268669f
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-12 14:23:43 +0530

    Andreas 03 template

---
+++ andreas03/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="wrap">
+            <header>
+                <div class="header">
+                    <div class="site-title">
+                        <h3>[<a href="">mohitesachin217</a>]</h3>
+                        <p>Your string site slogan!</p>
+                    </div>
+                    <div class="hero-contents">
+                        <h2>Speed and accessibility!</h2>
+                        <p>
+                            This website template is made for all of you who want a quick-loading and solid XHTML/CSS website with light use of images and a strong focus on accessibility. The template conforms to the WCAG 1.0 Triple-A rating, and it works in all common browser types.                    
+                        </p>
+                    </div>
+                </div>
+            </header>
+            <main class="main">
+                <div class="contents">
+                    <h3>The idea behind the design</h3>
+                    <p>
+                        The andreas03 template has a small file size, a simple layout for easy editing, good accessibility features and semantic markup. Five small images have been used for the design, adding only 3 kilobytes to the template load size (which is approximately 12kb).
+                    </p>
+
+                    <p>
+                        The images are linked from the stylesheet, 
+                        which means that they will not show up in browsers that does not support CSS. 
+                        They can also be safely removed, since the images do not affect the layout in any way. 
+                        In CSS-enabled browsers the removed images will be replaced by a background color. 
+                        In browsers with no CSS support, there will be no difference. See examples of this by looking at the alternate versions listed under the menu.
+                    </p>
+                    <p class="readmore">[<a href="">Back to Top</a>]</p>
+
+                    <h3>What is 'accessibility' and why is it important?</h3>
+                    <p>
+                        Accessibility is about making your website available and understandable for everyone, including users with disabilities. W3 explains it in a great way at the Web Accessibility Initiative.
+                    </p>
+                    <p>
+                        This template allows visitors to use the following accesskeys:
+                        1 - Back to top | 2 - Skip to site menu | 3 - Skip to main content | 4 - Return to index.html
+                    </p>
+                    <p>
+                        You can easily change these or add more accesskeys to make the site fit your needs, for example to allow easy access to each site menu link. Adding a logical tab order is recommended! And finally: the template allows browser-based font resizing, making sure that site visitors can enlarge the font size without breaking the page layout in any way.
+                    </p>
+                    <p class="readmore">[<a href="">Back to Top</a>]</p>
+                    <h3>Open source is freedom!</h3>
+                    <p>
+                            If you like this layout and would like to use it in any way, you are free to do so. 
+                            This design is released as open source, which means that you are free to make any changes you may want to. 
+
+                            I kindly ask that you leave the "Design by Andreas Viklund" link in the footer, but you are free to remove it if you want (or need) to. 
+                            If you use this design for commercial purposes, please consider making a symbolic donation through <a href="">my website</a>.
+                        </br>
+                        </br>
+                            Good luck with your new design!
+                        </br>
+                    </p>
+                    <p>Additional Information: Download more of my free website templates from <a href="">OSWD.org</a>!</p>
+                    <p class="readmore">[<a href="">Back to Top</a>]</p>
+                </div>
+                <div class="sidebar">
+                    <div class="menus">
+                        <a href="" class="menu-item">Site Menu:</a>
+                        <a href="" class="menu-item">First Page</a>
+                        <a href="" class="menu-item">Another Page</a>
+                        <a href="" class="menu-item">Number Three</a>
+                        <a href="" class="menu-item">Sample Button</a>
+                        <a href="" class="menu-item">Even more</a>
+                        <a href="" class="menu-item">And a last one</a>
+                    </div>
+                    <h3>Important note</h3>
+                    <p>
+                        This template conforms to Section 508 and to W3C's WCAG 1.0, level Triple-A.  </br>
+                        The code is valid XHTML 1.1 and CSS (no warnings).
+                    </p>
+                    <h3>Alternate versions</h3>
+                    
+                    Included in this template are the following example files:</br>
+                    </br>
+                    <a href="" class="sidebar-link" >- Main version </a> 
+                    <a href="" class="sidebar-link">-  No images version </a>
+                    <a href="" class="sidebar-link">-  No CSS version </a>
+                    </br>
+                    andreas03 v1.2 </br>
+                    (Nov 28, 2005) </br>
+
+                    <h4>Regular links</h4>
+                    <a href="" class="sidebar-link">- Free website templates</a>
+                    <a href="" class="sidebar-link">- Open Web Design</a>
+                    <a href="" class="sidebar-link">- OSWD.org</a>
+                    <a href="" class="sidebar-link">- Lagoona MP3s</a>
+            </main>
+                <footer class="footer">
+                © 2005 Your name. Design by Sachin Mohite </br>
+                Courtesy Open Web DesignThanks to Florida Vacation Homes
+                </footer> 
+            </div>
+</body>
+</html>
