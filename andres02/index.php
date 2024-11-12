7eb70f69261b2a84ee9e659f908279dc3b50af3d
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-12 09:09:58 +0530

    Andreas HTML template 02

---
+++ andres02/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="site_network_div">
+        Site Network:
+        <div class="site-network-block">Personal</div>
+        <div class="site-network-block">My Company</div>
+        <div class="site-network-block">Artist projects</div>
+        <div class="site-network-block">Shop</div>
+    </div>
+    <div class="wrap">
+        <header>
+            <div class="site-name">Mohitesachin217</div>
+            <div class="navitabs">
+                <div class="primary_menu activenavitab">Welcome</div>
+                <div class="primary_menu navitab">Bio</div>
+                <div class="primary_menu navitab">Discography</div>
+                <div class="primary_menu navitab">Downloads</div>
+                <div class="primary_menu navitab">Photos</div>
+                <div class="primary_menu navitab">Tour</div>
+                <div class="primary_menu navitab">Merchandise</div>
+                <div class="primary_menu navitab">Interview</div>
+                
+            </div>
+            <div class="hero_banner">
+                <div class="hero_contents"> 
+                    <h2> Welcome to andreas02! </h2>
+                    <p>
+                        This is my second website template. This time, I made a 2-column layout featuring two versions of my CSS tab menu. 
+                        The design is made with XHTML 1.1 and CSS, and it has no tables. 
+                        To give some suggestions on how the design can be used, I have filled it with some example content.
+                    </p>
+                    <p class="right"> <a>Read More...</a></p>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="contents-wrap">
+                <div class="contents-area ">
+                    <h3>The idea behind the design:</h3>
+                    <p>
+                        When it was time to redesign one of my sites, I got an idea about making one single design that all of my sites could use, and make some kind of official link network between the sites. My way of doing this turned out to be the use of toptabs (for selecting a website) and navitabs (for selecting subsections within the different sites). With a basic PHP template, all sites could now have the same design and share the same toptabs - but they can have their own separate content. If needed, each navitab can have a submenu in the sidebar, which allows this design to handle hundreds of pages while still keeping a quick and easy navigation. To make navigation more logical, each tab can have its own colorscheme. The "personal" site has blue colors, the "company" site is green, the "artist" site is red and so on...
+                    </p>
+
+                    <p class="content-notice">
+                        <strong>Please note:</strong> I have not aimed at making any kind of trendy or shiny look for this design. I prefer to keep it clean and simple, and let others add the details that will make the design special and unique. However, the design has some useful built-in features, such as a print CSS and full functionality when degrading to no-CSS mode. You can use the design as it is, but I recommend that you play around with it. Add things, remove things, let your creativity flow. Andreas02 gives you a solid structure to build from.
+                    </p>
+                    <h3>An open source design!</h3>
+                    <p>
+                        If you like this layout and would like to use it in any way, you are free to do so. 
+                        All I ask for is that you leave the "Design by Andreas Viklund" link in the footer of the site. 
+                        If you would like to remove that line, 
+                        or if you are interested in professional services that are related to webdesign or this layout (such as custom design, 
+                        branding, scripting or programming), 
+                        please contact me through 
+                        <a href="" style="text-decoration:none;">my website</a> for more information!
+                    </p>
+                    <p>Version: 1.2 (November 28, 2005)</p>
+                </div>
+                <div class="right-sidebar">
+                    <h3>More information:</h3>
+                    <p>
+                        Do you want more information about this design (and other designs I have released)? Then you should check out these pages:
+                    </p>
+                    <a href="sidebar-link">My Website</a>
+                    <a href="sidebar-link">More templates</a>
+                    <a href="sidebar-link">Open web design</a>
+                    <a href="sidebar-link">OSWD.org</a>
+                    <a href="sidebar-link">Valid XHTML</a>
+                    <a href="sidebar-link">Valid CSS</a>
+
+                    <h3>Image Example</h3>
+                    <img src="images/test.jpg" width="245px" class="img-class" />
+
+                    <h3>Wise Words</h3>
+                    <p>
+                    "It happens every day: information overload! Time for a reboot..."
+                    (traditional haiku poem)
+                    </p>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="footer-box">
+                    Copyright © 2005 Your Name. Design by mohite sachin.
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
