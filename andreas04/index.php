83f5f6fc65e9d69bdd0a910d0027d6677044b9a6
mohitesachin217 <mohitesachin217@gmail.com>
2024-11-26 18:49:17 +0530

    Andreas04 practice template
    Practice template

---
+++ andreas04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sitetitle">
+            <h1>Close to attraction</h1>
+            <div class="site-slogan">the andreas04 temple</div>
+        </div>
+        <header>
+        </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
+    </div>
+    
+</body>
+</html>
